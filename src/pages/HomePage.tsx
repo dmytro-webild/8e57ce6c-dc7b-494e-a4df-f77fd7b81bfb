067dc495b06c686319f0c8ab5e1d47ec8727042f
kudinDmitriyUp <kudin.dmitriy.up@gmail.com>
2026-08-08 04:27:30 +0000

    Bob AI: Remove the features section (including the features reveal /

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import HeroSection from './HomePage/sections/Hero';
 import AboutSection from './HomePage/sections/About';
-import FeaturesSection from './HomePage/sections/Features';
 import ProcessSection from './HomePage/sections/Process';
 import ProofSection from './HomePage/sections/Proof';
-import TestimonialsSection from './HomePage/sections/Testimonials';
 import FaqSection from './HomePage/sections/Faq';
 import ContactSection from './HomePage/sections/Contact';
 
-
 import TrustSection from './HomePage/sections/Trust';
 import ComparisonSection from './HomePage/sections/Comparison';
-import InspectionSection from './HomePage/sections/Inspection';export default function HomePage(): React.JSX.Element {
+import InspectionSection from './HomePage/sections/Inspection';
+
+export default function HomePage(): React.JSX.Element {
   return (
     <>
       <HeroSection />
@@ -19,10 +18,8 @@ import InspectionSection from './HomePage/sections/Inspection';export default fu
       <AboutSection />
       <ComparisonSection />
       <InspectionSection />
-      <FeaturesSection />
       <ProcessSection />
       <ProofSection />
-      <TestimonialsSection />
       <FaqSection />
       <ContactSection />
     </>
