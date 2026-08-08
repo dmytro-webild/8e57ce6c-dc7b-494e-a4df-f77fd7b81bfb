8fbb360ee6b3dcaedbb111f6d164abb483d0bc4d
bender <kudin.dima@gmail.com>
2026-08-08 04:23:28 +0000

    Switch to version 45: modified src/pages/HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import HeroSection from './HomePage/sections/Hero';
 import AboutSection from './HomePage/sections/About';
 import FeaturesSection from './HomePage/sections/Features';
+import ProcessSection from './HomePage/sections/Process';
 import ProofSection from './HomePage/sections/Proof';
 import TestimonialsSection from './HomePage/sections/Testimonials';
 import FaqSection from './HomePage/sections/Faq';
@@ -18,6 +19,7 @@ export default function HomePage(): React.JSX.Element {
       <AboutSection />
       <ComparisonSection />
       <FeaturesSection />
+      <ProcessSection />
       <ProofSection />
       <TestimonialsSection />
       <FaqSection />
